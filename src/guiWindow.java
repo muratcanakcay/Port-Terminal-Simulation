359c3c1aa20586e440c5ec8e2e266c8fc0a37794
Muratcan Akcay <muratcanakcay@gmail.com>
2022-08-28 14:15:58 +0200

    implement craneGrid to Gui

--- src/guiWindow.java
+++ src/guiWindow.java
@@ -14,6 +14,7 @@ public class guiWindow
     private JButton changeButton; // TODO: this is just for testing. remove later.
     private JLabel Jlabel2;
     private JScrollPane scrollPaneForConsole;
+    private JPanel craneGrid;
 
     private final JTextPane console;
     public JTextPane getConsole()
@@ -28,7 +29,61 @@ public class guiWindow
     {
         Border blackline = BorderFactory.createLineBorder(Color.black);
 
+        // initialize cellGrid
         cellGrid.setLayout(new GridLayout(rows, columns));
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                JPanel cell = new JPanel();
+                cell.setBorder(blackline);
+                cell.setLayout(new GridLayout(stackSize, 1));
+
+                for (int s = 0; s < stackSize; s++)
+                {
+                    JTextField stack = new JTextField();
+                    stack.setEditable(false);
+                    stack.setBackground(Color.WHITE);
+                    //stack.setText("Test: " + r + c);
+                    stack.setText("Empty");
+                    cell.add(stack);
+                }
+
+                cellGrid.add(cell);
+            }
+        }
+
+        // craneGrid
+        int noOfCranes = 2; // TODO: make this an argument
+        craneGrid.setLayout(new GridLayout(noOfCranes + 1, 5));
+
+        for (int r = 0; r < noOfCranes + 1; r++)
+        {
+            for (int c = 0; c < 5; c++)
+            {
+                JTextField crane = new JTextField();
+                crane.setEditable(false);
+                if (r != 0) crane.setBackground(Color.WHITE);
+                if (r == 0)
+                {
+                    Font boldCraneTextFont = new Font(crane.getFont().getName(), Font.BOLD, crane.getFont().getSize());
+                    crane.setFont(boldCraneTextFont);
+                    if (c==0) crane.setText("Crane");
+                    if (c==1) crane.setText("Status");
+                    if (c==2) crane.setText("Container");
+                    if (c==3) crane.setText("From");
+                    if (c==4) crane.setText("To");
+                }
+                else
+                {
+                    if (c==0) crane.setText("Crane0" + r);
+                }
+
+                craneGrid.add(crane);
+            }
+        }
+
+        // console area
         console = new JTextPane();
         console.setMargin(new Insets(20, 10, 55, 10));
         DefaultCaret caret = (DefaultCaret) console.getCaret();
@@ -37,40 +92,25 @@ public class guiWindow
         scrollPaneForConsole.getPreferredSize();
 
 
-        for (int r = 0; r < columns; r++)
-        {
-            for (int c = 0; c < rows; c++)
-            {
-                JPanel cell = new JPanel();
-                cell.setBorder(blackline);
 
-                cell.setLayout(new GridLayout(stackSize, 1));
-                for (int s = 0; s < stackSize; s++)
-                    cell.add(new JTextField());
-
-                cellGrid.add(cell);
-            }
-        }
-
-        Component[] cells = cellGrid.getComponents();
-        // iterate over cells...
-        int i = 0;
-        for (Component cellComponent : cells)
-        {
-            JPanel cell = ((JPanel)cellComponent);
-            Component[] stacks = cell.getComponents();
-
-            // iterate over stacks...
-            int j = 0;
-            for (Component stackComponent : stacks)
-            {
-                JTextField stack = ((JTextField)stackComponent);
-                stack.setText("Test: "+ i + j);
-                ++j;
-            }
-
-            ++i;
-        }
+//        Component[] cells = cellGrid.getComponents();
+//        // iterate over cells...
+//        int i = 0;
+//        for (Component cellComponent : cells)
+//        {
+//            JPanel cell = ((JPanel)cellComponent);
+//            Component[] stacks = cell.getComponents();
+//
+//            // iterate over stacks...
+//            int j = 0;
+//            for (Component stackComponent : stacks)
+//            {
+//                JTextField stack = ((JTextField)stackComponent);
+//                stack.setText("Test: "+ i + j);
+//                ++j;
+//            }
+//            ++i;
+//        }
 
         // TODO: Change to behaviour listening from cellAgent and changing Gui accordingly
         changeButton.addActionListener(new ActionListener()
@@ -88,6 +128,7 @@ public class guiWindow
                 JTextField stack = ((JTextField)stacks[2]);
 
                 stack.setText("Changed");
+                //stack.setBackground(Color.RED);
             }
         });
     }
